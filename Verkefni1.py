f1e8f5d05a284097f4d9ae59f14c26881d04b0c8
Qvorum <hakonha213@nemi.tskoli.is>
2018-01-19 15:01:18 +0000

    Update Verkefni1.py

--- Verkefni1.py
+++ Verkefni1.py
@@ -1,8 +1,16 @@
 from bottle import *
 import os
 
+
 @route('/')
 def index():
-    return "Halló heimur nú er gaman..."
-
+    return "<a href='/sida/1'>Hlekkur 1</a> , <a href='/sida/2'>Hlekkur 2</a> , <a href='/sida/3'>Hlekkur 3</a>"
+@route('/sida/<id>')
+def index(id):
+	return "Þetta er síða ", id
+def index():
+    return "Welkommen til jungelen!"
+@error(404)
+def villa(error):
+	return "<h2>Þessi síða er ekki til, reyndu aftur!</h2>"
 run(host='0.0.0.0', port=os.environ.get('PORT'))
